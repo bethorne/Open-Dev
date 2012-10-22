83062a35dd9ae902fef6c341a541374576e029e4
Ruperto Bugueño <ru.bugueno@gmail.com>
2012-10-22 00:50:26 -0300

    fixes #1 Datos requeridos

--- mensaje.php
+++ mensaje.php
@@ -9,7 +9,7 @@ case '1' :
 			echo "<table border='0'   width='400' bgcolor='#ddffdd'><tr><td align='left'> <table border='0'><tr><td valign='top'><img src='images/alerta.gif'></td><td><label id='alerta'>".$texto."</label></td></tr></table> </td></tr></table>";
 			break;
 case '2' :
-			echo "<table border='0'   width='400' bgcolor='#ddffdd'><tr><td align='left'> <table border='0'><tr><td valign='top'><img src='images/alerta.gif'></td><td><label id='alerta'>".$texto."</label></td></tr></table> </td></tr></table>";
+			echo "<table border='0'   width='400' bgcolor='#ffdddd'><tr><td align='left'> <table border='0'><tr><td valign='top'><img src='images/alerta.gif'></td><td><label id='alerta'>".$texto."</label></td></tr></table> </td></tr></table>";
 			break;
 }
 					
