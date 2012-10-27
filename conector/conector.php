5e21aafec78a9b402661b58fd0bc9cefb2baa084
Ruperto Bugueño <ru.bugueno@gmail.com>
2012-10-27 03:38:13 -0300

    conector a BD

--- conector/conector.php
+++ conector/conector.php
@@ -3,7 +3,7 @@
 		$servidor= "localhost";
 		$dbs="baro";
 		$login="root"; 
-		$pass= ""; 
+		$pass= "root"; 
 		
 		$conn=mysql_connect($servidor,$login ,$pass ) or die ('No Puedo conectar a la Base de Datos. Mensaje: ' . mysql_error());
 		mysql_select_db($dbs);
